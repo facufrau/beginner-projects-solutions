1c5dbd7c9d063960b4a912be5981715acdcc9fe6
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2020-06-23 13:51:57 -0300

    Updated magic ball, only uses command line interface.

--- magic_8ball.py
+++ magic_8ball.py
@@ -7,19 +7,10 @@ Allow the user to enter their question.
 Display an in progress message(i.e. "thinking").
 Create 20 responses, and show a random response.
 Allow the user to ask another question or quit.
-Bonus:
-    Add a gui.
-    It must have box for users to enter the question.
-    It must have at least 4 buttons:
-        ask
-        clear (the text box)
-        play again
-        quit (this must close the window)
 '''
 
 from time import sleep
 from random import choice
-from tkinter import *
 
 answers = [ 'It is certain.', ' It is decidedly so.', 'Without a doubt', 'Yes-definitely',
             'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.',
@@ -38,35 +29,10 @@ def answer():
     print(choice(answers))
     return 0
 
-'''while True:
+while True:
     answer()
     flag = input('Ask another question Yes/No?\n')
     if flag.lower() == 'Yes' or flag == 'y':
         continue
     else:
         break
-'''
-
-root = Tk()
-
-label = Label(text='Hello, Tkinter',
-        fg='white',     # Set text color to white
-        bg='#34A2FE',    # Background color to hex value.
-        width=10,
-        height=1
-        )
-label.pack()
-
-button = Button(
-    text='Click me!',
-    width=25,
-    height=5,
-    bg='blue',
-    fg='yellow',
-    )
-button.pack()
-
-entry = Entry(fg='yellow', bg='blue', width=50)
-entry.pack()
-
-root.mainloop()
