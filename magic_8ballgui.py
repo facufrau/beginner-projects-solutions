54484f7d958f9c8bf1a11b040207461b33d1874f
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2020-06-23 21:36:33 -0300

    Finished project
    Added warning message, all buttons working now.

--- magic_8ballgui.py
+++ magic_8ballgui.py
@@ -17,6 +17,7 @@ Bonus:
 from time import sleep
 from random import choice
 import tkinter as tk
+from tkinter import messagebox
 
 def ask():
     """Ask the question to the magic ball and insert the
@@ -28,24 +29,48 @@ def ask():
             'Cannot predict now.', 'Concentrate and ask again.', 'Don\'t count on it.', 'My reply is no.',
             'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 
-    lbl_answer["text"] = f'{choice(answers)}'
-    btn_ask["state"]='disabled'
+    question = ent_question.get()
+    
+    if question:
+        lbl_answer["text"] = f'{choice(answers)}'
+        btn_ask["state"] = 'disabled'
+    else:
+        messagebox.showwarning(message="Question field empty!", title="Empty field")
+
+def clear():
+    """
+    Clear and enable the text box.
+    """
+    ent_question.delete(0, 'end')
+
+def play_again():
+    """
+    Clear answer label and enable the ask button to play again.
+    """
+    lbl_answer["text"] = ''
+    btn_ask["state"] =  'normal'
+
+def close():
+    """
+    Exits the program.
+    """
+    window.destroy()
 
 window = tk.Tk()
 window.title('Magic ball')
 
 # Create a question frame: label and entry
 frm_question = tk.Frame(master=window)
-ent_question = tk.Entry(master=frm_question, width=60)
+ent_question = tk.Entry(master=frm_question,state='normal', width=60)
 lbl_question = tk.Label(master=frm_question, text='Your question: ')
 
 # Create buttons frame and organize.
 frm_buttons = tk.Frame(master=window)
 
-btn_ask = tk.Button(frm_buttons,state='normal', text='Ask!', command=ask)
-btn_clear = tk.Button(frm_buttons, text='Clear text box.')
-btn_again = tk.Button(frm_buttons, text='Play again')
-btn_quit = tk.Button(frm_buttons, text='Quit')
+btn_ask = tk.Button(frm_buttons, state='normal', text='Ask!', command=ask)
+btn_clear = tk.Button(frm_buttons, text='Clear text box', command=clear)
+btn_again = tk.Button(frm_buttons, text='Play again', command=play_again)
+btn_quit = tk.Button(frm_buttons, text='Quit', command=close)
 
 btn_ask.grid(row=0, column=0, padx=20, pady=10)
 btn_clear.grid(row=0, column=1, padx=20, pady=10)
