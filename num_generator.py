9c9d123643269624f1769ddf295b83d21b50c108
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2020-07-22 13:51:33 -0300

    Bug fixed
    Limited number input to 10000-99999

--- num_generator.py
+++ num_generator.py
@@ -6,16 +6,14 @@ while True:
     goal_num = input('Enter goal number: ')
     try:
         goal_num = int(goal_num)
-        if goal_num > 10000:
+        if goal_num > 10000 and goal_num < 100000:
             break
-        else:
-            print('Please enter a 5 digit integer number')
-            continue
+        print('Please enter a 5 digit integer number')
     except:
         print('That is not an integer number')
 
 print('This program will generate random numbers until reached the input number.')
-for i in range(3,0,-1):
+for i in [3,2,1]:
     print(f'{i}...')
     sleep(1)
 
