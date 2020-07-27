1c42fe9ceb8ea9aa6742b3eb6ed8657cdde9a1da
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2020-07-27 13:49:31 -0300

    Add files via upload

---
+++ solutions/changecalc.py
@@ -0,0 +1,52 @@
+# Beginner project 8
+# Change Calculator
+'''
+    Imagine that your friend is a cashier, but has a hard time counting back change to customers.
+    Create a program that allows him to input a certain amount of change,
+    and then print how how many quarters, dimes, nickels, and pennies
+    are needed to make up the amount needed. Example: if he inputs 1.47,
+    the program will say that he needs 5 quarters, 2 dimes, 0 nickels, and 2 pennies.
+    Subgoals:
+        So your friend doesn't have to calculate how much change is needed,
+        allow him to type in the amount of money given to him and the price of the item.
+        The program should then tell him the amount of each coin he needs like usual.
+        To make the program even easier to use, loop the program back to the top
+        so your friend can continue to use the program without having to close and open
+        it every time he needs to count change.
+'''
+while True:
+    try:
+        money = float(input('Please enter the money received: '))
+        if money > 0:
+            break
+    except:
+        print('Enter a valid amount of money (for example 100.50)')
+
+while True:
+    try:
+        price = float(input('Please enter the price of the object: '))
+        if price > 0:
+            break
+    except:
+        print('Enter a valid price (for example 35.66)')
+
+penny, nickel, dime, quarter = 0, 0, 0, 0
+change = round(money - price, 2)
+cents = change * 100
+
+while cents > 0:
+    if cents % 25 == 0:
+        quarter += 1
+        cents -= 25
+    elif cents % 10 == 0:
+        dime += 1
+        cents -= 10
+    elif cents % 5 == 0:
+        nickel += 1
+        cents -= 5
+    elif cents % 1 == 0:
+        penny += 1
+        cents -= 1
+
+print(f'The total change is {change}')
+print(f'You will need \n-->{quarter} quarter(s)\n-->{dime} dime(s)\n-->{nickel} nickel(s)\n-->{penny} penny(ies)')
--- solutions/coin_estimator.py
+++ solutions/coin_estimator.py
@@ -1,7 +1,39 @@
 # Beginner project 6 - Coin estimator by weight.
 
-coins = {'penny': {'name':'penny', 'weight': 2.500, 'fit': 50},
-        'nickel': {'name':'nickel', 'weight': 5.000, 'fit': 40},
-        'dime': {'name':'penny', 'weight': 2.268, 'fit': 50},
-        'quarter': {'name':'penny', 'weight': 5.670, 'fit': 40},
+from time import sleep
+
+coins = {'penny': {'weight': 2.500, 'fit': 50, 'value': 0.01},
+        'nickel': {'weight': 5.000, 'fit': 40, 'value': 0.05},
+        'dime': {'weight': 2.268, 'fit': 50, 'value': 0.10},
+        'quarter': {'weight': 5.670, 'fit': 40, 'value': 0.25},
         }
+
+# Ask the user for the total weight of each coin type.
+# Store the total weight for each coin type in the dictionary.
+for k in coins:
+    while True:
+        coin_weight = input(f'Enter the total weight of {k.capitalize()} in g (grams):\n')
+        try:
+            coin_weight = float(coin_weight)
+            if coin_weight > 0:
+                coins[k]['total_weight'] = coin_weight
+                break
+            else:
+                print('That is not a valid weight amount')
+        except:
+            print('That is not a valid weight amount')
+
+# Calculate total cash, how many coins and wrappers.
+# Format and print out the results.
+total_cash = 0
+
+for k in coins:
+    coins_qty = round(coins[k]['total_weight']/coins[k]['weight'])
+    wrappers = round(coins_qty/coins[k]['fit'])
+    total_cash += coins_qty * coins[k]['value']
+    sleep(1.5)
+    print(f'-----{k.upper()}-----')
+    print(f'You have {coins_qty} coins and you will need {wrappers} wrapper(s)')
+    print('----------------------')
+
+print(f'Your estimated total cash is {round(total_cash, 2)}')
