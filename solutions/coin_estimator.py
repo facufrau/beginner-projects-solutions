4512b70e1dd86f8f48af2124cec98fd9e158cd51
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2020-07-27 10:31:35 -0300

    Create coin_estimator.py

---
+++ solutions/coin_estimator.py
@@ -0,0 +1,7 @@
+# Beginner project 6 - Coin estimator by weight.
+
+coins = {'penny': {'name':'penny', 'weight': 2.500, 'fit': 50},
+        'nickel': {'name':'nickel', 'weight': 5.000, 'fit': 40},
+        'dime': {'name':'penny', 'weight': 2.268, 'fit': 50},
+        'quarter': {'name':'penny', 'weight': 5.670, 'fit': 40},
+        }
