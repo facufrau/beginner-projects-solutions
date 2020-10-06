cf33275d4400bb27f74a28769097a96ab7a5133b
Facundo Frau <51481449+facufrau@users.noreply.github.com>
2020-10-06 10:01:49 -0300

    Refactored with functions
    Added functions for more clarity and reuse.

--- solutions/what_is_my_number.py
+++ solutions/what_is_my_number.py
@@ -29,17 +29,35 @@ def check_prime(num):
         else:
             return True
 
-for i in range(1001):
-    digits = [int(x) for x in str(i)]
-    sum_digits = 0
-    if len(digits) >= 2:
-        if check_prime(i):
-            if (1 not in digits and 7 not in digits):
-                for x in digits:
-                    sum_digits += x
-                if sum_digits <= 10:
-                    if (digits[0] + digits[1]) % 2 != 0:
-                        if digits[-2] > 1 and digits[-2] % 2 == 0:
-                            if digits[-1] == len(digits):
-                                print(f'Number found! - {i}')
-                                break
+def split_digits(num):
+    '''
+    Takes a number and returs a list of its digits.
+    '''
+    digits = [int(x) for x in str(num)]
+    return digits
+
+def add_digits(number_list):
+    '''
+    Calculate the sum of a list of numbers.
+    '''
+    total = 0
+    for x in number_list:
+        total += x
+    return total 
+
+def main():    
+    for i in range(1001):
+        digits = split_digits(i)
+        sum_digits = add_digits(digits)
+        if len(digits) >= 2:
+            if check_prime(i):
+                if (1 not in digits and 7 not in digits):
+                    if sum_digits <= 10:
+                        if (digits[0] + digits[1]) % 2 != 0:
+                            if digits[-2] > 1 and digits[-2] % 2 == 0:
+                                if digits[-1] == len(digits):
+                                    print(f'Number found! - {i}')
+                                    break
+
+if __name__ == "__main__":
+    main()
