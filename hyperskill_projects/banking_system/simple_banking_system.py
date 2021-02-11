a3ee79ab574fc34cce00546f9d0863b461d10da1
Facundo Frau <facufrau@gmail.com>
2021-02-10 21:33:51 -0300

    Finished stage 4-4

--- hyperskill_projects/banking_system/simple_banking_system.py
+++ hyperskill_projects/banking_system/simple_banking_system.py
@@ -6,18 +6,19 @@ import sqlite3
 conn = sqlite3.connect('card.s3db')
 # Create table
 cur = conn.cursor()
-cur.execute('DROP TABLE IF EXISTS card')
+cur.execute('CREATE TABLE IF NOT EXISTS card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);')
 conn.commit()
-cur.execute('CREATE TABLE card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);')
-conn.commit()
+
 
 def main():
     """Main function for the app."""
-    menu = "1. Create an account\n2. Log into account\n0. Exit"
-    submenu = "1. Balance\n2. Log out\n0. Exit"
+    menu = ["1. Create an account", "2. Log into account", "0. Exit"]
+
+    submenu = ["1. Balance", "2. Add income", "3. Do transfer",
+               "4. Close account", "5. Log out", "0. Exit"]
 
     while True:
-        print(menu)
+        print('\n'.join(menu))
         option = int(input())
         id_num = 0
         if option == 1:
@@ -32,27 +33,77 @@ def main():
             cur.execute("SELECT number, pin, balance FROM card WHERE number=(?)", (user_card,))
             conn.commit()
             response = cur.fetchone()
-            
-            print(response)
+
             if not response or response[1] != user_pin:
                 print("\nWrong card number or PIN!\n")
             else:
                 print("\nYou have successfully logged in!\n")
                 while True:
-                    print(submenu)
+                    cur.execute("SELECT balance FROM card WHERE number=(?)", (user_card,))
+                    conn.commit()
+                    balance = cur.fetchone()[0]
+
+                    print('\n'.join(submenu))
                     sub_option = int(input())
+                    
+                    # Check balance option
                     if sub_option == 1:
-                        balance = response[2]
                         print(f"\nBalance: {balance}\n")
+                    
+                    # Add cash to account
                     elif sub_option == 2:
+                        cash = int(input("\nEnter income:\n"))
+                        cash = cash + balance
+                        cur.execute("UPDATE card SET balance=(?) WHERE number=(?)", (cash, user_card))
+                        conn.commit()
+                        print("Income was added!\n")
+
+                    # Transfer to another account.
+                    elif sub_option == 3:
+                        print("\nTransfer")
+                        destination_card = input("Enter card number:\n")
+                        if not checksum(destination_card):
+                            print("Probably you made a mistake in the card number. Please try again!\n")
+                        else:
+                            cur.execute("SELECT balance FROM card WHERE number=(?)", (destination_card,))
+                            conn.commit()
+                            if not cur.fetchone():
+                                print("Such a card does not exist.\n")
+                            else:
+                                cur.execute("SELECT balance FROM card WHERE number=(?)", (destination_card,))
+                                conn.commit()
+                                dest_balance = cur.fetchone()[0]
+                                transfer = int(input("Enter how much money you want to transfer:\n"))
+                                if transfer > balance:
+                                    print("Not enough money!\n")
+                                else:
+                                    sender = balance - transfer
+                                    receiver = dest_balance + transfer
+                                    cur.execute("UPDATE card SET balance=(?) WHERE number=(?)", (sender, user_card))
+                                    cur.execute("UPDATE card SET balance=(?) WHERE number=(?)", (receiver, destination_card))
+                                    conn.commit()
+                                    print("Success!\n")
+
+                    # Close account and delete from database.
+                    elif sub_option == 4:
+                        cur.execute("DELETE FROM card WHERE number=(?)", (user_card,))
+                        conn.commit()
+                        print("\nThe account has been closed!\n")
+                        break
+
+                    # Log out
+                    elif sub_option == 5:
                         print("\nYou have successfully logged out!\n")
                         break
+
+                    # Exit program
                     elif sub_option == 0:
                         print("\nBye!")
                         exit()
         elif option == 0:
             print("\nBye!")
             exit()
+
 
 def create_acc():
     """ Generates a credit card number and pin """
@@ -73,7 +124,7 @@ def create_acc():
         card += str(0)
     else:
         card += str(10 - checksum % 10)
-        
+
     pin = str(random.randint(0, 10000)).zfill(4)  # 4 digit pin (0000-9999)
     print(f"\nYour card has been created")
     print(f"Your card number:\n{card}")
@@ -81,6 +132,22 @@ def create_acc():
     print()
 
     return card, pin
+
+
+def checksum(ccnumber):
+    """ Checks the validity of a card"""
+    card_num_list = [int(x) for x in ccnumber]
+    check = 0
+    for i, n in enumerate(card_num_list):
+        if i % 2 == 0:
+            current = n * 2
+            if current > 9:
+                current -= 9
+            check += current
+        else:
+            check += n
+    return (check % 10) == 0
+
 
 if __name__ == '__main__':
     main()
